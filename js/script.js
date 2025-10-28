7321fbb7e8534a6f3bd7458586e8d3a4a914a227
StivenMastrovito <stivenmastrovito6@gmail.com>
2025-10-28 16:04:49 +0100

    feat: risoluzione

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Consegna:
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per?
+// Abbiamo visto qualcosa di particolare che possiamo usare?
+
+// Consigli del giorno:
+// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+
+// ESECUZIONE LOGICA
+// FOR DA 1 A 100, POI UN PRIMO IF PER VEDERE SE è MULTIPLO SIA DI 3 E DI 5,
+//  POI 2 ELSE IF PER VERIFICARE SE SONO MULTIPLI DI 3 O DI 5 SOLTANTO CON L'UTILIZZO DI PERCENTUALE
+
+for(i=1; i <= 100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    }else if(i % 3 === 0){
+        console.log("Fizz");        
+    }else if(i % 5 === 0){
+        console.log("Buzz");
+    }else{
+        console.log(i);        
+    }
+}
